9814558864048a3006d185f901da305dd7734e71
Geert Jan Bex <geertjan.bex@uhasselt.be>
2019-01-17 21:07:53 +0100

    Add autolink extension load

--- source/conf.py
+++ source/conf.py
@@ -42,6 +42,7 @@ extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
     'sphinx.ext.ifconfig',
+    'sphinx.ext.autosectionlabel',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
