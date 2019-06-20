af8f0b4eecb79c27a9823e19b7e473af56943796
Geert Jan Bex <geertjan.bex@uhasselt.be>
2019-06-20 19:50:52 +0200

    Add logo and change style

--- source/conf.py
+++ source/conf.py
@@ -78,13 +78,18 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'classic'
+html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'logo': 'logo.webp',
+    'logo_name': True,
+    'sidebar_width': '200pt',
+    'page_width': '1200pt',
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
